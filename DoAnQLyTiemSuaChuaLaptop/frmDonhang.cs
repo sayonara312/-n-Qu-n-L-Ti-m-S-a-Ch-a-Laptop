afe2564b15719ce85e29c16cfebe2e01f0736539
luna312 <42026843+luna312@users.noreply.github.com>
2020-08-01 15:38:36 +0700

    donhang

--- DoAnQLyTiemSuaChuaLaptop/frmDonhang.cs
+++ DoAnQLyTiemSuaChuaLaptop/frmDonhang.cs
@@ -17,9 +17,9 @@ namespace DoAnQLyTiemSuaChuaLaptop
         {
             InitializeComponent();
         }
-        DataTable tblDONHANG;
-        DataTable tblMaCTHD;
+        DataTable tblDONHANG,tblCTHD;
         SqlDataAdapter daDH;
+        BindingManagerBase bindDH;
         private void dgvDonHang_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
         {
             foreach (DataGridViewRow r in dgvDonHang.Rows)
@@ -27,32 +27,63 @@ namespace DoAnQLyTiemSuaChuaLaptop
                 r.Cells[0].Value = r.Index + 1;
             }
         }
-        BindingManagerBase bindDH;
+        private void loadDSKH()
+        {
+            
+        }
         bool capnhat;
-        private void loadDSNV()
+        private void addColCTHD()
+        {
+            DataSet ds = new DataSet();
+            ds.Tables.AddRange(new DataTable[] { tblDONHANG, tblCTHD });
+            DataRelation qh = new DataRelation("FRK_DONHANG_CTHD", tblDONHANG.Columns["MaDH"], tblCTHD.Columns["MaDH"]);
+            ds.Relations.Add(qh);
+            DataColumn cot_SoTien = new DataColumn("SoTien", Type.GetType("System.String"), "Parent(FRK_DONHANG_CTHD).SoTien");
+            DataColumn cot_TenDichVu = new DataColumn("TenDichVu", Type.GetType("System.String"), "Parent (FRK_DONHANG_CTHD).TenDichVu");
+            DataColumn cot_HinhThucSua = new DataColumn("HinhThucSua", Type.GetType("System.String"), "Parent (FRK_DONHANG_CTHD).HinhThucSua)");
+            DataColumn cot_TrangThai = new DataColumn("TrangThai", Type.GetType("System.String"), "Parent (FRK_DONHANG_CTHD).TrangThai)");
+            tblCTHD.Columns.Add(cot_SoTien);
+            tblCTHD.Columns.Add(cot_TenDichVu);
+            tblCTHD.Columns.Add(cot_HinhThucSua);
+            tblCTHD.Columns.Add(cot_TrangThai);
+        }
+        private void loadDONHANG()
         {
             tbMaDH.DataBindings.Add("text", tblDONHANG, "MaNV", true);
-            tbSDT.DataBindings.Add("text", tblDONHANG, "SoDT", true);
             tbTenKH.DataBindings.Add("text", tblDONHANG, "TenNV", true);
-            tbKTS.DataBindings.Add("text", tblMaCTHD, "HinhThucSua", true);
-            tbTenMay.DataBindings.Add("text", tblDONHANG, "Email", true);
-            dateNLap.DataBindings.Add("text", tblDONHANG, "NgayLap", true);
+            dateNLap.DataBindings.Add("value", tblDONHANG, "NgayLa", true);
             tbTenMay.DataBindings.Add("text", tblDONHANG, "TenMay", true);
             tbTTM.DataBindings.Add("text", tblDONHANG, "TrinhTrangMay", true);
-            cbTrangThai.DataBindings.Add("text", tblMaCTHD, "TrangThai", true);
-            tbTenDV.DataBindings.Add("text", tblMaCTHD, "TenDichVu", true);
-            tbSoTien.DataBindings.Add("text", tblMaCTHD, "SoTien", true);
             bindDH = this.BindingContext[tblDONHANG];
-            bindDH = this.BindingContext[tblMaCTHD];
-
         }
+
         private void loadKH()
         {
             bindDH = this.BindingContext[tblDONHANG];
             dgvDonHang.AutoGenerateColumns = false;
             dgvDonHang.DataSource = tblDONHANG;
 
+        }
 
+        private void frmDonhang_Load(object sender, EventArgs e)
+        {
+            tblDONHANG = new DataTable();
+            tblCTHD = new DataTable();
+            daDH = new SqlDataAdapter("Select * from DONHANG", Modules.cnnStr);
+            try
+            {
+                daDH.Fill(tblDONHANG);
+
+                SqlCommandBuilder cmdDH = new SqlCommandBuilder(daDH);
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            loadDONHANG();
+            addColCTHD();
+            capnhat = false;
         }
     }
 }
